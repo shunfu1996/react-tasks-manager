d88550c7815fd2f753b3b32b85d30ecff6916d6e
gabriel888chow <94506701+gabriel888chow@users.noreply.github.com>
2022-01-26 18:32:13 +0800

    commit jan26

--- finalproject/src/Components/Data/test.js
+++ finalproject/src/Components/Data/test.js
@@ -32,8 +32,36 @@ describe('The app', function() {
     
 // }
 
+// const shallow = require('enzyme/shallow');
+// const rewire = require('rewire');
+// const sinon = require('sinon');
 
-expect(
-    Array.isArray(allTasks),
-    'The `allTasks` prop passed to `<TasksList />` should be an array after adding a new event',
-  ).to.be.true;
+// describe('', function () {
+//     let appModule;
+//     try {
+//       appModule = rewire('../Data/Data');
+//     } catch (e) {
+//       expect(
+//         true,
+//         'Try checking your code again. You likely have a syntax error.',
+//       ).to.equal(false);
+//     }
+//     const AppFunction = appModule.__get__('Data');
+  
+//     it('', function () {
+//       let wrapper;
+//       try {
+//         wrapper = shallow(<Data />);
+//       } catch (e) {
+//         expect(
+//           false,
+//           "Check for errors in your `Data` component, it isn't rendering",
+//         );
+//       }
+      
+    //   expect(
+    //       Array.isArray(allTasks),
+    //       'The `allTasks` prop passed to `<TasksList />` should be an array after adding a new event',
+    //     ).to.be.true;
+//     });
+// });
