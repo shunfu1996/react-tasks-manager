4e2f22bf3c3228081ad684ceeb6315eb5cdcbea6
gabriel888chow <94506701+gabriel888chow@users.noreply.github.com>
2022-01-31 02:43:19 +0800

    commit jan31

--- finalproject/src/Components/Data/Data.js
+++ finalproject/src/Components/Data/Data.js
@@ -38,6 +38,12 @@ const Data = ({id, name, description, assignedTo, dueDate, deleteTask, submittin
     
 
     // }
+    const [editing, setEditing] = useState(false)
+
+    const handleEdit = () =>{
+        setEditing(!editing)
+        console.log(editing)
+    }
     
 
     async function deleteItem() {  //the button of delete task  function 
@@ -47,7 +53,7 @@ const Data = ({id, name, description, assignedTo, dueDate, deleteTask, submittin
                 return prev.filter(item => item.id !== id)
             })
         } else {
-            const result = await r("Are you sure to delete?");
+            const result = await confirm("Are you sure to delete?");
             if (result) {
             deleteTask(function(prev) {
                 return prev.filter(item => item.id !== id)
@@ -71,22 +77,32 @@ const Data = ({id, name, description, assignedTo, dueDate, deleteTask, submittin
     //the date format
     return(
         <div className="card row midShape nopadding place dateShape" /* style={{display: isDisplay}} */>
-            <div className="card-header text-center" id="myList" role="tablist" >Task Name: {name} </div>
+            <div className="card-header text-center" id="myList" role="tablist" style={editing?{display: "none"}:{display: "block"}} >Task Name: {name} </div>
+            <div style={editing?{display: "block"}:{display: "none"}}>
+            <span>Task Name: </span><input className="card-header text-center" id="myList" role="tablist" />
+            </div>
                 <div className="d-flex justify-content-around">
                 {/* <p className="list-group-item list-group-item-action d-flex justify-content-center" data-toggle="list" href="#home" role="tab">Task Name: {name}</p>   */}
                 {/* <p className="list-group d-flex justify-content-center" role="tab">Task Name: {name}</p>   */}
                 {/* <button onClick={deleteItem}>delete</button> */}
                 {/* <div className="tab-content"> */}
-                <div className="card-body">
-                    <div className="tab-pane active" id="home" role="tabpanel">
+                <div className="card-body" >
+                    <div className="tab-pane active" id="home" role="tabpanel" style={editing?{display: "none"}:{display: "block"}}>
                         <p className=" d-flex text-left">Due Date: {dueDate}</p>
                         <p className=" d-flex text-left">Type: {assignedTo}</p>
                         <p className=" d-flex text-left">Description: {description}</p>
                     </div>
+                    <div className="tab-pane active" id="home" role="tabpanel" style={editing?{display: "block"}:{display: "none"}}>
+                        <span>Due Date:</span><input className=" d-flex text-left" type="text" value="name" />
+                        <span>Type:</span><input className=" d-flex text-left" type="text" value="name" />
+                        <span>Description:</span><input className=" d-flex text-left" type="text" value="name" />
+                        {/* <input className=" d-flex text-left" value="edting" />
+                        <input className=" d-flex text-left" value="edting" /> */}
+                    </div>
                     <div className="Button">
-                    {/* <button className="btn" onClick={edit}>Edit</button> */}
-                    <button className="toDo btn text-white" onClick={handleStatus} style={{backgroundColor: color}}>{status}</button>
-                    <button className="btn btn btn-outline-secondary" type="button" onClick={() => deleteItem()}>Delete</button>
+                        <button className="btn edit" onClick={handleEdit}>{editing?"Ok":"Edit"}</button>
+                        <button className="toDo btn text-white" onClick={handleStatus} style={{backgroundColor: color}}>{status}</button>
+                        <button className="btn btn btn-outline-secondary" type="button" onClick={() => deleteItem()}>Delete</button>
                     </div>
                 </div>                    
             </div>                    
--- finalproject/src/Components/Header/Header.js
+++ finalproject/src/Components/Header/Header.js
@@ -4,17 +4,18 @@ import { API_GET_DATA } from '../Server/API'; */
 import './Header.css';
 import Calendar from  '../Calendar/Calendar'
 import React, {useState} from 'react'
-
+//const || var || let  isScheduler = false <<js  hook>> const [isScheduler, setIsScheduler] = useState(false)
+//                     isScheduler = 12eqw <<js  hook>> setIsScheduler(12eqw)
 
 const Header = ({ CardData, setFilterTask, filterTask}) => {
     const [dateState, setDateState] = useState(new Date());
-    const [isScheduler, setTest] = useState(false)
+    const [isScheduler, setIsScheduler] = useState(false)
     var today = new Date();
 
     var nowDate = today.getFullYear()+'-'+("0" + (today.getMonth() + 1)).slice(-2)+'-'+today.getDate();
  
     const handleIsScheduler = () =>{
-        setTest(!isScheduler)
+        setIsScheduler(!isScheduler)
         setFilterTask([])
     }
 
@@ -59,11 +60,16 @@ const Header = ({ CardData, setFilterTask, filterTask}) => {
                     <button type="button" className="btn btn-primary buttonShape">SORT</button>
                 </div> */}
                 <div className="text-center">
-                    <button onClick={handleIsScheduler}>{isScheduler?"list":"Scheduler"}</button>
-                    <img src="https://img.icons8.com/clouds/100/000000/todo-list.png" id="Check" alt="Check" width="120"/>
-                    {/* <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-todo-list/check1.webp" alt="Check" width="60" /> */}
-                    <p className="title text-center text-uppercase fw-bold topic">To Do List</p>
+                {/* <i class="bi bi-calendar-check"></i> */}
+                        <button className="button1 btn list" onClick={handleIsScheduler}>{isScheduler?"List":"Scheduler"}</button>
                     </div>
+                    <div className="text-center">
+                        <img src="https://img.icons8.com/clouds/100/000000/todo-list.png" id="Check" alt="Check" width="120"/>
+                    </div>
+                    {/* <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-todo-list/check1.webp" alt="Check" width="60" /> */}
+                    <div>
+                    <p className="title text-center text-uppercase fw-bold topic">To Do List</p>
+                </div>
                     {/* <div className="text-center col-3 pt-5">
                     <button  type="button" className="btn btn-primary buttonShape">ALL</button>
                 </div> */}
@@ -71,33 +77,22 @@ const Header = ({ CardData, setFilterTask, filterTask}) => {
             <div style={isScheduler?{display: "none"}:{display: "block"}}>
                 <div className="row m-3" >
                     <div className="col-3 d-grid gap-2; ">
-                        <button type="button" className="button1 btn" onClick={handleYesterday}>Yesterday</button>
+                        <button type="button" className="button1 btn" onClick={handleYesterday}>Past</button>
                     </div>
                     <div className="col-3 d-grid gap-2">
-                        <button type="button" className="button2 btn"  onClick={handleToday} >Today</button>
+                        <button type="button" className="button2 btn" onClick={handleToday}>Today</button>
                     </div>
                     <div className="col-3 d-grid gap-2">
-                        <button type="button" className="button3 btn" onClick={handleTomorrow}>Tomorrow</button>
+                        <button type="button" className="button3 btn" onClick={handleTomorrow}>Future</button>
                     </div>
                     <div className="col-3 d-grid gap-2">
-                        <button type="button" className="button3 btn" onClick={handleAll}>ALL</button>
+                        <button type="button" className="button3 btn" onClick={handleAll}>All</button>
                     </div>
                 </div>
-<<<<<<< HEAD
-                <div className="col-3 d-grid gap-2">
-                    <button type="button" className="button2 btn"  onClick={handleToday}>Today</button>
-                </div>
-                <div className="col-3 d-grid gap-2">
-                    <button type="button" className="button3 btn" onClick={handleTomorrow}>Tomorrow</button>
-                </div>
-                <div className="col-3 d-grid gap-2">
-                    <button type="button" className="button3 btn" onClick={handleAll}>ALL</button>
-                </div>
-            </div>
-=======
-            </div>    
-            <Calendar isScheduler={isScheduler} dateState={dateState} setDateState={setDateState} CardData={CardData} filterTask={filterTask} setFilterTask={setFilterTask}/>
->>>>>>> 4a318d9b02cd01c82657b8ba35e84b38260ce528
+            </div>  
+                <div className="calendar"> 
+                    <Calendar isScheduler={isScheduler} dateState={dateState} setDateState={setDateState} CardData={CardData} filterTask={filterTask} setFilterTask={setFilterTask}/>
+                </div> 
         </div>
     )
 }
