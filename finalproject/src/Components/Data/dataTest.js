6543823c8e5477a22a73805f19acc373581d9b33
gabriel888chow <94506701+gabriel888chow@users.noreply.github.com>
2022-02-11 02:07:28 +0800

    commit test

---
+++ finalproject/src/Components/Data/dataTest.js
@@ -0,0 +1,65 @@
+function submitEdits(id) {  // edit function map 左之前的 array
+    submittingStatue.current = true;
+    const updatedTodos = [...filterTask].map((todo) => {
+      if (todo.id === id) {
+        todo.name = editName;
+        todo.description = editDescription;
+        todo.type = editType;
+        todo.dueDate = editDate;
+      }
+      return todo;
+    });
+    deleteTask(updatedTodos);
+    // setTodoEditing(null);
+    setEditing(!editing)
+    console.log(editing);
+  }
+
+  function clickEdit() { 
+    setEditing(!editing) // click edit 時反轉
+  }
+
+  function addItem(e) {
+    const errorMessage = document.querySelector('#error');
+    e.preventDefault(); // to prevent the web F5
+    if(!validInput(name)){
+        errorMessage.innerHTML = "Please enter the name!"; //check the input is correct
+        errorMessage.style.display = "block";
+    } else if(!validInput(description)){
+        errorMessage.innerHTML = "Please enter the description!";
+        errorMessage.style.display = "block";
+    }  else if(!validInput(type)){
+        errorMessage.innerHTML = "Please choose a type!";
+        errorMessage.style.display = "block";
+        console.log(type)
+    }  else if(!validInput(dueDate)){
+        errorMessage.innerHTML = "Please choose a date!";
+        errorMessage.style.display = "block";
+    }  else{
+        errorMessage.style.display = "none";
+        submittingStatue.current = true ;
+        add(function(prevData){
+            return [
+                {   
+                    id: v4(),
+                    name,
+                    description,
+                    type,
+                    dueDate,
+                },
+                ...prevData,
+            ];
+        })    
+            setDueDate("");
+            setType("");
+            setDescription("");
+            setName("");
+    }    
+  }
+
+
+  
+
+module.exports = submitEdits, clickEdit, addItem, deleteItem
+
+
