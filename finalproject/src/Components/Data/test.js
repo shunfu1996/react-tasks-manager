38ba4b9f27817a03b2f201daaf45189710609d84
Brian chow <brianchowshunfu@gmail.com>
2022-02-11 05:45:16 +0800

    testing

--- finalproject/src/Components/Data/test.js
+++ finalproject/src/Components/Data/test.js
@@ -1,67 +1,16 @@
-// var assert = chai.assert,
-//     expect = require('../Data/Data');
-// describe('The app', function() {
-//     describe('this feature', function() {
-//         it("is a function", function(){
-//             assert.throw(iThrowError(), Error, "Error thrown");
-//         });
-//     });
-// });
+const submitEdits = require ('./Datatest');
+const clickEdit = require('./Datatest');
+const addItem = require('./Datatest');
+const deleteItem = require('./Datatest');
 
-// var expect = require('chai').expect;
-// const add = require('../Data/Data');
-// describe('test', () => {
-//     it('should respond 200',function(){
-//         assert.throw(deleteItem(), Error, "Error thrown");
-//     });
-// });
+test ('submitEdits function exists', () => {
+    expect(typeof submitEdits).toEqual('function');
+});
 
+test ('clickEdit function exists', () => {
+    expect(typeof clickEdit).toEqual('function');
+});
 
-
-// var expect = require('chai').expect;
-// import ('../Data/Data');
-// describe('test', handleStatus() => {
-//   it('delete button', () => {
-//     expect(handleStatus).to.be.equal(setColor("red"));
-//   });
-// });
-
-// import test from '../Data/Data';
-
-// describe("Test suit", function () {
-    
-// }
-
-// const shallow = require('enzyme/shallow');
-// const rewire = require('rewire');
-// const sinon = require('sinon');
-
-// describe('', function () {
-//     let appModule;
-//     try {
-//       appModule = rewire('../Data/Data');
-//     } catch (e) {
-//       expect(
-//         true,
-//         'Try checking your code again. You likely have a syntax error.',
-//       ).to.equal(false);
-//     }
-//     const AppFunction = appModule.__get__('Data');
-  
-//     it('', function () {
-//       let wrapper;
-//       try {
-//         wrapper = shallow(<Data />);
-//       } catch (e) {
-//         expect(
-//           false,
-//           "Check for errors in your `Data` component, it isn't rendering",
-//         );
-//       }
-      
-    //   expect(
-    //       Array.isArray(allTasks),
-    //       'The `allTasks` prop passed to `<TasksList />` should be an array after adding a new event',
-    //     ).to.be.true;
-//     });
-// });
+test ('addItem function exists', () => {
+    expect(typeof addItem).toEqual('function');
+});
